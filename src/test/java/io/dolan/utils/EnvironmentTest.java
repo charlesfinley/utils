2ceb4717f706ca436cd29dec01eafb0c4da1a0a9
charlesfinley <GImprunner12!@>
2020-05-26 21:50:07 -0400

    Updated unit tests

--- src/test/java/io/dolan/utils/EnvironmentTest.java
+++ src/test/java/io/dolan/utils/EnvironmentTest.java
@@ -13,4 +13,11 @@ public class EnvironmentTest {
         Execute.run(() -> Environment.fromString(null)).shouldThrow(IllegalArgumentException.class);
     }
 
+    @Test
+    public void testPrependEnv() {
+        final String userId = "1234";
+        assertEquals("D1234", Environment.fromString("dev").prependEnv(userId));
+        assertEquals("Q1234", Environment.fromString("qa").prependEnv(userId));
+        assertEquals("P1234", Environment.fromString("prod").prependEnv(userId));
+    }
 }
