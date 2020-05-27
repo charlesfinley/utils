03cfd706c73f520290b2f07221cfd48e578afbe1
charlesfinley <GImprunner12!@>
2020-05-27 09:10:18 -0400

    Random things for Travis CI and Codecov.io testing

--- src/main/java/io/dolan/utils/App.java
+++ src/main/java/io/dolan/utils/App.java
@@ -2,6 +2,10 @@ package io.dolan.utils;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Something...");
+        System.out.println("Result = " + getSum(1, 2));
+    }
+
+    public static int getSum(int a, int b) {
+        return a + b;
     }
 }
